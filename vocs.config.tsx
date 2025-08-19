83944789c108fede8bb3e9a2f81b5079877553a5
Sola Ogunsakin <juniorsola@gmail.com>
2025-08-18 22:13:48 -0400

    updates

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -36,4 +36,23 @@ export default defineConfig({
       link: "/kudos",
     },
   ],
+  head() {
+    return (
+      <>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QWRKRKBJZZ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QWRKRKBJZZ');`,
+          }}
+        />
+      </>
+    );
+  },
 });
